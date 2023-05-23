48cffd3ce6a8f008b83cae8b7ef9199fa95a36e3
shikizi sykes <rizalmasan4@gmail.com>
2023-05-23 18:04:15 +0700

    update code

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 """
-Ini adalah demo project pertama dengan python
+Semua sintaksis dasar bahasa pemrograman terdiri dari
+1. Sekuensial: Langkah berurutan
+2. Percabangan: Langkah melompat jika kondisi terpenuhi
+3. Perulangan: Mengulang langkah yang sama berkali-kali selama/sampai kondisi terpenuhi
 """
-print("Hello world!")
-print("My name is rizal")
+
+# Sekuensial
+print('Ibu berkata, "Pergi ke toko"')
+print('Budi menjawab, "baik, apa yang harus saya lakukan di toko"')
+print('ibu menjawab, "beli satu botok susu, dan jika ada telor beli 6"')
+print('maka budi berangkat ke toko')
+print('Dan mulai berbelanja')
+
+# Percabangan
